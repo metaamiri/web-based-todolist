f6671c6c8638a335ce9c0d43c67a09788d6dbd5d
metaamiri <mohammadamiri007i@gmail.com>
2025-02-16 15:08:22 +0330

    change navbar and add forms

--- list/views.py
+++ list/views.py
@@ -1,3 +1,20 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from .forms import NewTaskForm
 
-# Create your views here.
+def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"]=[]
+        
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            request.session["tasks"] += [task]
+            return HttpResponseRedirect(reverse("list:index"))
+        else:
+            return render(request, "list/index.html", {"form":form})
+    else:
+        form = NewTaskForm()
+    return render(request, "list/index.html", {"form":form})
