282deeaee1fd89ab1f8a9b76392cf31066bdfc94
metaamiri <mohammadamiri007i@gmail.com>
2025-02-20 16:54:26 +0330

    fixing finished tasks (not complete)

--- list/urls.py
+++ list/urls.py
@@ -5,4 +5,5 @@ app_name = "list"
 urlpatterns = [
     path("", views.index, name="index"),
     path("clear", views.clear, name="clear"),
+    path('save-finished-tasks/', views.save_finished_tasks, name='save_finished_tasks'),
 ]
--- list/views.py
+++ list/views.py
@@ -2,10 +2,13 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from .forms import NewTaskForm
+from django.http import JsonResponse
+import json
 
 def index(request):
     if "tasks" not in request.session:
         request.session["tasks"]=[]
+        request.session["finished_tasks"] = []
         
     if request.method == "POST":
         form = NewTaskForm(request.POST)
@@ -14,11 +17,29 @@ def index(request):
             request.session["tasks"] += [task]
             return HttpResponseRedirect(reverse("list:index"))
         else:
-            return render(request, "list/index.html", {"form":form,"tasks":request.session["tasks"]})
+            return render(request, "list/index.html", {"form":form,"tasks":request.session["tasks"], "finished_tasks":request.session["finished_tasks"]})
     else:
         form = NewTaskForm()
-    return render(request, "list/index.html", {"form":form,"tasks":request.session["tasks"]})
+    return render(request, "list/index.html", {"form":form,"tasks":request.session["tasks"], "finished_tasks":request.session["finished_tasks"]})
 
 def clear(request):
     request.session["tasks"] = []
     return HttpResponseRedirect(reverse("list:index"))
+
+
+def save_finished_tasks(request):
+    if request.method == "POST":
+        data = json.loads(request.body)  # دریافت داده‌های JSON
+        request.session["finished_tasks"] += data.get("finished_tasks", [])  # استخراج تسک‌ها از داده‌ها
+
+        # اینجا می‌توان داده‌ها را در دیتابیس ذخیره کرد
+        print("Received Finished Tasks:", request.session["finished_tasks"]) 
+        for task in request.session["tasks"]:
+            for ftask in request.session["finished_tasks"]:
+                if task == ftask:
+                    request.session["tasks"].remove(task) 
+
+        return JsonResponse({"status": "success", "message": "Tasks updated successfully!"})
+
+    return JsonResponse({"status": "error", "message": "Invalid request!"}, status=400)
+
